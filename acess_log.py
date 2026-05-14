295220c9c78e89316dc0504bbd4bd6b77883c2b7
João Pedro S R Lima <joaopedrosrlima@hotmail.com>
2026-05-14 11:56:12 -0300

    fix: forcando try except de escrita no log

--- acess_log.py
+++ acess_log.py
@@ -1,14 +1,9 @@
 def registrar_tentativa(nome_api, rota, ip, autorizado):
-    # Traduz o booleano para um texto legível no log
     status = "AUTORIZADO" if autorizado else "NEGADO"
-    
-    # Esse print vai aparecer perfeito no painel Functions da Vercel para você monitorar
     print(f"[LOG ACESSO] API: {nome_api} | Rota: {rota} | IP: {ip} | Status: {status}")
     
     try:
-        # Funciona na sua máquina local salvando o arquivo texto
         with open("tentativas_acesso.log", "a", encoding="utf-8") as arquivo:
             arquivo.write(f"API: {nome_api} | Rota: {rota} | IP: {ip} | Status: {status}\n")
     except OSError:
-        # Evita que a API caia por causa do sistema Read-only da Vercel
         pass
