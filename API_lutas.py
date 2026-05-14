8161094ecc343a239004f700e2571d69bde4b386
João Pedro S R Lima <joaopedrosrlima@hotmail.com>
2026-05-14 00:31:59 -0300

    Render database bugs fix

--- API_lutas.py
+++ API_lutas.py
@@ -10,7 +10,7 @@ from acess_log import registrar_tentativa
 from models import IntegradorAutorizado, Base
 
 # 1. Configuração do Banco (Lutas)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./lutas_agendadas.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database_v2.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
